68c3ea24f73c561bf33d0a2fa439029b2e5b0c2b
Anton Leshkovich <antonleshkovich@mail.ru>
2023-01-25 01:19:08 +0300

    fix: fixed bugs index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ module.exports = function reverse (n) {
     let absValue = Math.abs(n); 
     let arr = Array.from(absValue.toString());
     let reverseValue = arr.reduce(function (previousValue, item) {
-        return Number(`${item}${previousValue}`);
+        return `${item}${previousValue}`;
     });
-    return reverseValue;
+    let result = Number(reverseValue);
+    return result;
 }
