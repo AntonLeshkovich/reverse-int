ed1445e278e85f6b175e2e6a4967a1478271d662
Anton Leshkovich <antonleshkovich@mail.ru>
2023-01-25 00:48:31 +0300

    feat: add solution index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let arr = Array.from(n.toString());
+    let reverseValue = arr.reduce(function (previousValue, item) {
+        return Number(`${item}${previousValue}`);
+    });
+    return reverseValue;
 }
